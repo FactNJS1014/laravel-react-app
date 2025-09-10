0a1c3b1ffee3331cf3e9fd67983e02b13aea6d57
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-09-10 13:10:03 +0700

    use icons

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom";
 import Router from "../router/Index.jsx";
+import { IoHome , IoInformationCircle  } from "react-icons/io5";
+
 function App() {
+    const active = "px-3 bg-purple-600 text-white rounded-xs";
 
     return (
         <div>
@@ -12,24 +15,24 @@ function App() {
                     About
                 </NavLink>
             </nav> */}
-            <div className="relative p-3 bg-gray-100">
+            <div className="relative p-5 bg-gray-100">
                 Menu
-                <div className="absolute top-0 right-0 p-3">
+                <div className="absolute top-0 right-0 items-center p-3">
                     <div className="flex gap-3">
                         <NavLink
                             to="/"
-                            className={"px-3 bg-purple-300 rounded-xs"}
+                            className={({ isActive }) => (isActive ? "px-4 py-1 bg-blue-600 text-white rounded-xs" : "px-4 py-1 bg-blue-300 rounded-xs")}
                         >
-                            Home
+                            <IoHome className="inline text-2xl" />
                         </NavLink>
                         <NavLink
                             to="/about"
-                            className={"px-3 bg-purple-300 rounded-xs"}
+                            className={({ isActive }) => (isActive ? "px-4 py-1 bg-blue-600 text-white rounded-xs" : "px-4 py-1 bg-blue-300 rounded-xs")}
 
                         >
-                            About
+                            <IoInformationCircle className="inline text-2xl" />
                         </NavLink>
-                        <NavLink className={"px-3 bg-purple-300 rounded-xs"}>
+                        <NavLink className={({ isActive }) => (isActive ? "px-4 py-1 bg-blue-600 text-white rounded-xs" : "px-4 py-1 bg-blue-300 rounded-xs")} to="/contact">
                             Contact
                         </NavLink>
                     </div>
