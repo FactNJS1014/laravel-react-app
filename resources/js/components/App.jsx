dd0c0ad5512e62a1976a894600d867bdc1989ef5
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-09-10 11:53:07 +0700

    design react view

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -1,16 +1,40 @@
-import { NavLink } from "react-router-dom"
-import Router from "../router/Index.jsx"
+import { NavLink } from "react-router-dom";
+import Router from "../router/Index.jsx";
 function App() {
-  return (
+
     <div>
-      <h1>Hello App</h1>
-        <nav>
-            <NavLink to="/" className={"text-2xl font-bold"}>Home</NavLink>
-            <NavLink to="/about" className={"text-2xl font-bold ms-3"}>About</NavLink>
-        </nav>
-        <Router/>
-    </div>
-  )
+        {/* <nav>
+                <NavLink to="/" className={"text-2xl font-bold"}>
+                    Home
+                </NavLink>
+                <NavLink to="/about" className={"text-2xl font-bold ms-3"}>
+                    About
+                </NavLink>
+            </nav> */}
+        <div className="relative p-3 bg-gray-100">
+            Menu
+            <div className="absolute top-0 right-0 p-3">
+                <div className="flex gap-3">
+                    <NavLink to="/" className={"px-3 bg-purple-300 rounded-xs"}>
+                        Home
+                    </NavLink>
+                    <NavLink
+                        to="/about"
+                        className={"px-3 bg-purple-300 rounded-xs"}
+
+                    >
+                        About
+                    </NavLink>
+                    <NavLink className={"px-3 bg-purple-300 rounded-xs"}>
+                        Contact
+                    </NavLink>
+                </div>
+            </div>
+        </div>
+        <div className="p-3">
+            <Router />
+        </div>
+    </div>;
 }
 
-export default App
+export default App;
